6376cf026beed2cac83fd74b71d4d5fd59b994bb
jorgiom4 <gorkalqy@hotmail.com>
2020-02-07 09:30:10 +0100

    Primer commit del pryecto

---
+++ src/main/java/com/instancia2/servicio/commons/Application.java
@@ -0,0 +1,16 @@
+package com.instancia2.servicio.commons;
+
+// import org.springframework.boot.SpringApplication; --> Quitamos el import al no usar SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	/*
+	Quitamos el método main al tratarse de un proyecto de librerio y no de un aplicacón que se ejecute
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+	}
+	*/
+}
